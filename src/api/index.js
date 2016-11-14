b7f6552789cfb407d6a35fdacbb87f6a93c28f89
walid1992 <541280314@qq.com>
2016-11-14 13:09:05 +0800

    格式化代码

--- src/api/index.js
+++ src/api/index.js
@@ -6,26 +6,41 @@
 
 import Vue from 'vue'
 import Axios from 'axios'
+import QS from 'querystring'
 import Call from 'api/call'
 
 function use () {
   Vue.use(Axios)
-  // baseURL
+
+  // TODO baseURL
   // Axios.defaults.baseURL = 'http://103.37.145.195:8830'
-  // 设置超时时间
+
+  // TODO 设置超时时间
   Axios.defaults.timeout = 2000
+
+  // TODO http code 校验
   Axios.defaults.validateStatus = function (status) {
     return status
   }
 
-  // 添加请求conig配置
+  // TODO GET 请求 params 序列化
+  Axios.defaults.paramsSerializer = function (params) {
+    return QS.stringify(params)
+  }
+
+  // TODO 设置POST等请求 body 序列化
+  Axios.defaults.transformRequest = [function (body) {
+    return QS.stringify(body)
+  }]
+
+  // TODO 添加请求conig配置
   // Axios.interceptors.request.use(config => {
   //   return config
   // }, error => {
   //   return Promise.reject(error)
   // })
 
-  // 设置统一请求拦截
+  // TODO 设置统一请求拦截
   Axios.interceptors.response.use(response => {
     if (response.data.code === 0) {
       console.log('请求成功 : \n', response)
@@ -39,9 +54,14 @@ function use () {
   })
 }
 
-function get (url, body) {
+/**
+ * @description 统一 GET 请求
+ * @param url
+ * @param params --> GET 请求参数（***?name=walid&age=25）
+ */
+function get (url, params) {
   return new Call((resolve, reject) => {
-    Axios.get(url, {params: body})
+    Axios.get(url, {params: params})
       .then(response => {
         resolve(response.data)
       })
@@ -51,9 +71,14 @@ function get (url, body) {
   })
 }
 
+/**
+ * @description 统一 POST 请求
+ * @param url
+ * @param body --> POST 请求 data
+ */
 function post (url, body) {
   return new Call((resolve, reject) => {
-    Axios.post(url, {params: body})
+    Axios.post(url, body)
       .then(response => {
         resolve(response.data)
       })
---
+++ src/api/user/index.js
@@ -0,0 +1,22 @@
+/**
+ * @author walid
+ * @description commonApi 模块
+ * @date 2016/11/09
+ */
+
+import Api from 'api'
+
+const apiURL = {
+  // TODO 卖家注册
+  signin: '/v1/user/signin'
+}
+
+module.exports = {
+  signin: function (mobile, mobileCode, extendCode) {
+    return Api.post(apiURL.signin, {
+      'mobile': mobile,
+      'mobileCode': mobileCode,
+      'extendCode': extendCode
+    })
+  }
+}
