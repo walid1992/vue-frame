7d982e2a70e644dff9dcdd7f9dd1d07a776e5856
walid <541280314@qq.com>
2017-03-26 00:01:06 +0800

    提交项目框架

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,58 @@
 module.exports = {
+  env: {
+    'browser': true,
+    'commonjs': true,
+    'es6': true
+  },
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    'sourceType': 'module'
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
+  globals: {
+    'Vue': false
+  },
+  // extends: 'standard',
   plugins: [
     'html'
   ],
-  globals: {
-    Vue: true
-  },
-
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  rules: {
+    // no-console
+    'no-console': 0,
+    // no-var
+    'no-var': 'error',
+    // 要求或禁止 var 声明中的初始化
+    'init-declarations': 2,
+    // 强制使用单引号
+    'quotes': ['error', 'single'],
+    // 要求或禁止使用分号而不是 ASI
+    'semi': ['error', 'never'],
+    // 禁止不必要的分号
+    'no-extra-semi': 'error',
+    // 强制使用一致的换行风格
+    'linebreak-style': ['error', 'unix'],
+    // 空格2个
+    'indent': ['error', 2, {'SwitchCase': 1}],
     // 指定数组的元素之间要以空格隔开(,后面)， never参数：[ 之前和 ] 之后不能带空格，always参数：[ 之前和 ] 之后必须带空格
-    "array-bracket-spacing": [2, "never"],
+    'array-bracket-spacing': [2, 'never'],
+    // 在块级作用域外访问块内定义的变量是否报错提示
+    'block-scoped-var': 0,
+    // if while function 后面的{必须与if在同一行，java风格。
+    'brace-style': [2, '1tbs', {'allowSingleLine': true}],
+    // 双峰驼命名格式
+    'camelcase': 2,
+    // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，
+    // always-multiline：多行模式必须带逗号，单行模式不能带逗号
+    'comma-dangle': [2, 'never'],
     // 控制逗号前后的空格
-    "comma-spacing": [2, { "before": false, "after": true }],
-    "indent": [2, 2, { "SwitchCase": 1 }],
-    //语句强制分号结尾
-    //"semi": [2, "always"],
-    "no-unused-vars": 0,
-    //禁止修改const声明的变量
-    "no-const-assign": 2,
-    //函数定义时括号前面要不要有空格
-    "space-before-function-paren": [0, "never"],
-    "no-new": 0,//禁止在使用new构造一个实例后不赋值
-    "no-control-regex": 0,//禁止在正则表达式中使用控制字符
-    "no-div-regex": 0,//不能使用看起来像除法的正则表达式/=foo/
-    "no-empty-character-class": 0,//正则表达式中的[]内容不能为空
-    "no-invalid-regexp": 0,//禁止无效的正则表达式
-    "no-regex-spaces": 0,//禁止在正则表达式字面量中使用多个空格 /foo bar/
-    "no-useless-escape": 0,
-    "no-unneeded-ternary": 0,
-    "eqeqeq": 0,
+    'comma-spacing': [2, {'before': false, 'after': true}],
+    // 控制逗号在行尾出现还是在行首出现
+    'comma-style': [2, 'last'],
+    // 圈复杂度
+    'complexity': [2, 20],
+    // 以方括号取对象属性时，[ 后面和 ] 前面是否需要空格, 可选参数 never, always
+    'computed-property-spacing': [2, 'never'],
+    // TODO 关闭 强制方法必须返回值，TypeScript强类型，不配置
+    // 'consistent-return': 0
   }
 }
